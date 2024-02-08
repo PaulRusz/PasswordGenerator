b2c2bb740568816c8e067812baaff5cb70fddfbd
PaulRusz <paulruszkay@gmail.com>
2024-02-08 10:10:13 -0500

    added comments to JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Assignment code here
-//passwordOptions has all the necessary data required to generate the password requested
 
+//passwordOptions has all the necessary data required to generate the password requested
 function generatePassword() {
+  //these variables contain all the characters that will be chosen for the password
   var password = ""
   var chosenCharacters = ""
   var userPasswordLength = prompt("Choose a length for your password between 8 and 128 characters");
@@ -9,6 +9,7 @@ function generatePassword() {
   var confirmLower = confirm("Do you want any lowercase letters?")
   var confirmNumbers= confirm("Do you want any numbers?")
   var confirmSpecial = confirm("Do you want any special characters?")
+
   if (confirmUpper === true) {
     chosenCharacters = chosenCharacters + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   }
@@ -22,18 +23,18 @@ function generatePassword() {
     chosenCharacters = chosenCharacters + "!@#$%^&*()"
   }
 
-
+//If the user selects a password < 8 or > 128 a prompt will appear to let the user know they need to select a number between 8 & 128
   if (userPasswordLength < 8 || userPasswordLength > 128) {
     alert ("Invalid password length.  Please choose a password length between 8 and 128.")
     var userPasswordLength = prompt("Choose a length for your password between 8 and 128 characters")
   } 
-
+//if a user fails to put in a character type, a message will appear to remind the user to select a user type for their password
   if (confirmUpper === false && confirmLower === false 
   && confirmNumbers === false && confirmSpecial === false) {
     window.alert("Please choose a character type for your password");
   }
   
-
+//randomly selects the chosen characters based off the users input
   for (let i = 0; i < userPasswordLength; i++) {
     password = password + chosenCharacters.charAt(Math.floor(Math.random()* chosenCharacters.length))
   }
