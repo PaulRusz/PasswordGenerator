0f9f43abcd891ca8a0003849a141378e055af202
PaulRusz <paulruszkay@gmail.com>
2024-02-07 20:28:01 -0500

    added code to JS

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
 // Assignment code here
+//passwordOptions has all the necessary data required to generate the password requested
+
+function generatePassword() {
+  var password = ""
+  var chosenCharacters = ""
+  var userPasswordLength = prompt("Choose length of password between 8 and 128 characters");
+  var confirmUpper = confirm("Do you want uppercase letters?")
+  var confirmLower = confirm("Do you want lowercase letters?")
+  var confirmNumbers= confirm("Do you want any numbers?")
+  var confirmSpecial = confirm("Do you want any special characters?")
+  if (confirmUpper === true) {
+    chosenCharacters = chosenCharacters + "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  }
+  if (confirmLower === true) {
+    chosenCharacters = chosenCharacters + "abcdefghijklmnopqrstuvwxyz"
+  }
+  if (confirmNumbers === true) {
+    chosenCharacters = chosenCharacters + "0123456789"
+  }
+  if (confirmSpecial === true) {
+    chosenCharacters = chosenCharacters + "!@#$%^&*()"
+  }
+
+  for (let i = 0; i < userPasswordLength; i++) {
+    password = password + chosenCharacters.charAt(Math.floor(Math.random()* chosenCharacters.length))
+  }
+  return password;
+}
 
 
 // Get references to the #generate element
@@ -17,3 +45,5 @@ console.log(writePassword);
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
